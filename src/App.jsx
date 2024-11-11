57946bf5d886d47edafb3e8384cb5bb023e65fe1
Nishad <mohdnishadmp@gmail.com>
2024-11-11 13:23:57 +0400

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./sections/Home";
 import Projects from "./sections/Projects";
 import SmoothScrollLayout from "./components/SmoothScrollLayout";
 import About from "./sections/About";
 import Contacts from "./sections/Contacts";
+import Loading from "./sections/Loading";
 
 const App = () => {
+
+  const [loading,setLoading] = useState(true);
+
+  useEffect(()=>{
+    const handleLoad = () => {
+      setLoading(false);
+    }
+
+    window.addEventListener("load",handleLoad);
+
+    return () => window.removeEventListener("load",handleLoad);
+  },[])
   
   return (
-    <SmoothScrollLayout>
+    <>
+    {loading ?
+      (<Loading/>) :
+      (<SmoothScrollLayout>
       <Home />
       <Projects />
       <About/>
       <Contacts/>
-    </SmoothScrollLayout>
+    </SmoothScrollLayout>)
+    }
+    </>
+    
+   
   );
 };
 
