3918add211577315dcb6dfe9214e3f6af86de1ba
Nishad <mohdnishadmp@gmail.com>
2024-11-11 13:27:34 +0400

    loading deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Home from "./sections/Home";
 import Projects from "./sections/Projects";
 import SmoothScrollLayout from "./components/SmoothScrollLayout";
 import About from "./sections/About";
 import Contacts from "./sections/Contacts";
-import Loading from "./sections/Loading";
 
 const App = () => {
-
-  const [loading,setLoading] = useState(true);
-
-  useEffect(()=>{
-    const handleLoad = () => {
-      setLoading(false);
-    }
-
-    window.addEventListener("load",handleLoad);
-
-    return () => window.removeEventListener("load",handleLoad);
-  },[])
   
   return (
-    <>
-    {loading ?
-      (<Loading/>) :
-      (<SmoothScrollLayout>
+    <SmoothScrollLayout>
       <Home />
       <Projects />
       <About/>
       <Contacts/>
-    </SmoothScrollLayout>)
-    }
-    </>
-    
-   
+    </SmoothScrollLayout>
   );
 };
 
