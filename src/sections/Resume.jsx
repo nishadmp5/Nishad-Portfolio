f5e8a4df86e503d3ba1c81f38dbe28bb125ed091
Nishad <mohdnishadmp@gmail.com>
2024-11-28 21:27:39 +0400

    edit resume

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
   return (
     <div className='fixed right-0 top-[36%] bg-[#9bd4d7] text-white px-2 lg:px-3 py-1 lg:py-1.5 z-30'>
-        <a download="NISHAD_FRONTENDDEV" href="/nishad_cv.pdf">
+        <a download="nishad_frontenddev.pdf" href="/nishad_cv.pdf">
             <h2 className='[writing-mode:vertical-rl] [text-orientation:upright] text-[1rem] lg:text-lg font-bold lg:font-[900] tracking-[-0.15em]'>DOWNLOAD</h2>
             <h2 className='font-bold text-[1rem] lg:text-lg mt-0.5 lg:mt-1'>CV</h2>
         </a>
